02d89463af669a60a6d86aa550ffc1719eea062a
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2020-10-26 16:44:15 +0000

    Initial content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Agnieszka Pas</h1>
+        <div className="links">
+          <a
+            href="https://github.com/agapas"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/agnieszka-pas-26131b101/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
+        </div>
       </header>
+      <div className="content">some content</div>
     </div>
   );
 }
