453bd5b540c64599807c839732a355523808393e
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2020-10-26 19:08:15 +0000

    Added font awesome for icons in links

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ function App() {
       <header className="header">
         <h1>Agnieszka Pas</h1>
         <div className="links">
+          <a href="mailto:agnieszka_pas@yahoo.ie">
+            <i className="fa fa-fw fa-envelope fa-lg" />
+          </a>
           <a
             href="https://github.com/agapas"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Github
+            <i className="fa fa-fw fa-github fa-lg" />
           </a>
           <a
             href="https://www.linkedin.com/in/agnieszka-pas-26131b101/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Linkedin
+            <i className="fa fa-fw fa-linkedin-square fa-lg" />
           </a>
         </div>
       </header>
