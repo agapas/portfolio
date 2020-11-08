52c058c410f022b8e4cc14269732e74f688e6008
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2020-11-08 20:57:59 +0000

    Added data structure and TODO

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import { Info } from "components/header/Info";
 import { Links } from "components/header/Links";
 import "./App.css";
 
+// TODO:
+// move data to resorces and change App into class component with fetching data
+const data = {
+  name: "Agnieszka Pas",
+  occupation: "Front-End Software Engineer",
+  "contact-info": {
+    email: "agnieszka_pas@yahoo.ie",
+    github: "https://github.com/agapas",
+    linkedin: "https://www.linkedin.com/in/agnieszka-pas-26131b101/",
+  },
+  projects: {},
+};
+
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
